318db4e76cfe32bcdcb9fdf458cd7aa95be0ed42
haukli <hli@tango.me>
2016-01-06 16:38:52 +0800

    Use the abbreviation(Ex) for the class name instead of Exercise.

--- $1Fundamentals/$1_1BasicProgrammingModel/Ex1.java
+++ $1Fundamentals/$1_1BasicProgrammingModel/Ex1.java
@@ -3,7 +3,7 @@ package $1Fundamentals.$1_1BasicProgrammingModel;
 /**
  * Created by hailstone01 on 1/6/16.
  */
-public class Exercise1 {
+public class Ex1 {
     public static void main(String[] args) {
         System.out.println((0 + 15) / 2);
         System.out.println(2.0e-6 * 100000000.1);
---
+++ $1Fundamentals/$1_1BasicProgrammingModel/Ex3.java
@@ -0,0 +1,7 @@
+package $1Fundamentals.$1_1BasicProgrammingModel;
+
+/**
+ * Created by hailstone01 on 1/6/16.
+ */
+public class Ex3 {
+}
