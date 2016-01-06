dc0c8eec3759190550d798e21801301e3bc92ecb
haukli <hli@tango.me>
2016-01-06 15:15:54 +0800

    add several directories, some exercises for chapter 1.1

---
+++ $1Fundamentals/$1_1BasicProgrammingModel/Exercise2.java
@@ -0,0 +1,11 @@
+package $1Fundamentals.$1_1BasicProgrammingModel;
+
+/**
+ * Created by hailstone01 on 1/6/16.
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println((1 + 2.236) / 2);
+        System.out.println(1 + 2 + 3 + 4.0);
+    }
+}
