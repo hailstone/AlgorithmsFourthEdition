c05c62e8af01a67edc91bcc1a791e2829d52b5c3
haukli <haukli@qq.com>
2016-01-09 17:25:04 +0800

    exercises for chapter 1.2

---
+++ $1_Fundamentals/$1_2_DataAbstraction/Accumulator.java
@@ -0,0 +1,37 @@
+package $1_Fundamentals.$1_2_DataAbstraction;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Scanner;
+
+/**
+ * Created by hailstone01 on 1/9/16.
+ */
+public class Accumulator {
+    private double total;
+    private int N;
+
+    public void addDataValue(double val) {
+        N++;
+        total += val;
+    }
+
+    public double mean() {
+        return total / N;
+    }
+
+    public String toString() {
+        return "Mean (" + N + " values): " + String.format("%7.5f", mean());
+    }
+
+
+    public static void main(String[] args) {
+        int T = new Scanner(System.in).nextInt();
+        Accumulator a = new Accumulator();
+
+        for (int t = 0; t < T; t++) {
+            a.addDataValue(StdRandom.random());
+        }
+        System.out.println(a);
+    }
+}
