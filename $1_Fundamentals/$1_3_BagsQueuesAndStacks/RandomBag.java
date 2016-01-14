67544a6f4bf633d8d0ce12588aa32bc946ea935f
haukli <haukli@qq.com>
2016-01-14 20:50:26 +0800

    exercises for chapter 1.3  ex34: random bag

---
+++ $1_Fundamentals/$1_3_BagsQueuesAndStacks/RandomBag.java
@@ -0,0 +1,62 @@
+package $1_Fundamentals.$1_3_BagsQueuesAndStacks;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by hailstone01 on 1/14/16.
+ */
+public class RandomBag<Item> implements Iterable<Item> {
+    private ArrayList<Item> arrayList = new ArrayList<Item>();
+
+    public boolean isEmpty() {
+        return arrayList.isEmpty();
+    }
+
+    public int size() {
+        return arrayList.size();
+    }
+
+    public void add(Item item) {
+        arrayList.add(item);
+    }
+
+    public void print() {
+        for (Item item : arrayList) {
+            System.out.println(item);
+        }
+    }
+
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<Item> {
+        ArrayList<Item> temp = new ArrayList<Item>(arrayList);
+
+        @Override
+        public boolean hasNext() {
+            return !temp.isEmpty();
+        }
+
+        @Override
+        public Item next() {
+            int randomIndex = (int) (Math.random() * temp.size());
+            return temp.remove(randomIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomBag<String> randomBag = new RandomBag<String>();
+        randomBag.add("aa");
+        randomBag.add("bb");
+        randomBag.add("cc");
+        randomBag.add("dd");
+
+        for (String s : randomBag) {
+            System.out.println(s);
+        }
+    }
+}
