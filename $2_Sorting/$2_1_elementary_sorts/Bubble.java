6ee4a2f3de2dcefcdf824492ba0cc9e336df2710
haukli <haukli@qq.com>
2016-01-21 20:18:50 +0800

    exercises for chapter2.1

---
+++ $2_Sorting/$2_1_elementary_sorts/Bubble.java
@@ -0,0 +1,31 @@
+package $2_Sorting.$2_1_elementary_sorts;
+
+/**
+ * Created by hailstone01 on 1/20/16.
+ */
+public class Bubble {
+    public static void sort(double[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] + arr[j + 1];
+                    arr[j + 1] = arr[j] - arr[j + 1];
+                    arr[j] = arr[j] - arr[j + 1];
+                }
+            }
+        }
+    }
+
+    public static void print(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        double[] arr = {5, 3, 7, 9, 3, 1, 6, 3, 8, 2};
+        sort(arr);
+        print(arr);
+    }
+}
