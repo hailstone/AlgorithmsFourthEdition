767717f6fedba8dafb04d061e9285a11b04ae5bf
haukli <haukli@qq.com>
2016-02-17 21:15:03 +0800

    some exercises

---
+++ $3_Searching/TT.java
@@ -0,0 +1,16 @@
+package $3_Searching;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hailstone01 on 2/17/16.
+ */
+public class TT {
+    private int i;
+    private TT tt;
+
+    public static void main(String[] args) {
+        TT t = new TT();
+        ArrayList arrayList = new ArrayList();
+    }
+}
